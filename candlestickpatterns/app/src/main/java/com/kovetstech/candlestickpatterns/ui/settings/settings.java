55e56b26295d2c90e1322539ed306447c57ddee7
shay alon vash <shayalon.business@gmail.com>
2023-01-23 18:39:03 +0200

    Created Settings Package

--- candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/settings.java
+++ candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/settings.java
@@ -1,4 +1,4 @@
-package com.kovetstech.candlestickpatterns;
+package com.kovetstech.candlestickpatterns.ui.settings;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.kovetstech.candlestickpatterns.R;
 
 /**
  * A simple {@link Fragment} subclass.
