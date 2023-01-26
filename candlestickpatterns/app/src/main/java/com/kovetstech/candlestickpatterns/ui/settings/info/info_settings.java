41893a130a2ac34c7b71aeef65a80307076d11a9
shay alon vash <shayalon.business@gmail.com>
2023-01-26 17:12:51 +0200

    Added settings windows

---
+++ candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/info/info_settings.java
@@ -0,0 +1,30 @@
+package com.kovetstech.candlestickpatterns.ui.settings.info;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kovetstech.candlestickpatterns.R;
+
+public class info_settings extends Fragment {
+
+    public info_settings() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_settings, container, false);
+    }
+
+
+}
---
+++ candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/simulator/simulator_settings_color_picker.java
@@ -0,0 +1,121 @@
+package com.kovetstech.candlestickpatterns.ui.settings.simulator;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+import com.kovetstech.candlestickpatterns.MainActivity;
+import com.kovetstech.candlestickpatterns.R;
+
+import static android.content.res.Configuration.UI_MODE_NIGHT_NO;
+import static android.content.res.Configuration.UI_MODE_NIGHT_YES;
+import static com.kovetstech.candlestickpatterns.MainActivity.context;
+
+public class simulator_settings_color_picker extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+
+    int chosen_color;
+    private String colorpicker_title;
+    private static Context c;
+
+    private TextView title;
+    private ImageView color_circle;
+
+    public simulator_settings_color_picker() {
+        // Required empty public constructor
+    }
+    public static simulator_settings_color_picker newInstance(String param1) {
+        simulator_settings_color_picker fragment = new simulator_settings_color_picker();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            colorpicker_title = getArguments().getString(ARG_PARAM1);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_simulator_settings_color_picker, container, false);
+
+        title = v.findViewById(R.id.colorpicker_title);
+        color_circle = v.findViewById(R.id.colorpicker_circle);
+
+        setColors();
+        set();
+
+        return v;
+    }
+
+    public void set(){
+        SharedPreferences prefs = getContext().getSharedPreferences("CSPPREFS", Context.MODE_PRIVATE);
+        int chosen_color = prefs.getInt(colorpicker_title+"c", Color.WHITE); //0 is the default value
+
+        title.setText(colorpicker_title);
+        color_circle.setImageTintList(ColorStateList.valueOf(chosen_color));
+
+        color_circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_colorpicker();
+            }
+        });
+    }
+    public void setColors(){
+        c = context;
+        c.setTheme(R.style.Widget_Custom_AlertDialog);
+    }
+    public void open_colorpicker(){
+        Context c = context;
+        context.setTheme(R.style.AlertDialog_AppCompat);
+
+        ColorPickerDialogBuilder
+                .with(c)
+                .setTitle("Choose color")
+                .initialColor(Color.WHITE)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        set_new_color(selectedColor);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
+    public void set_new_color(int color){
+        chosen_color = color;
+
+        color_circle.setImageTintList(ColorStateList.valueOf(chosen_color));
+
+        SharedPreferences prefs = getContext().getSharedPreferences("CSPPREFS", Context.MODE_PRIVATE);
+        prefs.edit().putInt(colorpicker_title+"c", chosen_color).commit();
+    }
+}
