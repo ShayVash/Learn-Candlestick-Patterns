c2375c7c5527889f192821327ce1cddd5308fd72
ShayVash <shayalon.business@gmail.com>
2023-01-28 12:03:52 +0200

    .

--- candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/simulator/simulator_settings.java
+++ candlestickpatterns/app/src/main/java/com/kovetstech/candlestickpatterns/ui/settings/simulator/simulator_settings.java
@@ -3,6 +3,7 @@ package com.kovetstech.candlestickpatterns.ui.settings.simulator;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,19 +14,19 @@ import com.kovetstech.candlestickpatterns.ui.settings.sub_settings_button;
 
 public class simulator_settings extends Fragment {
 
+    View v;
+
     public simulator_settings() {
         // Required empty public constructor
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_simulator_settings, container, false);
+        v = inflater.inflate(R.layout.fragment_simulator_settings, container, false);
 
         simulator_settings_color_picker positive_colorpicker = simulator_settings_color_picker.newInstance("Bullish Candle Color");
         getChildFragmentManager().beginTransaction().replace(R.id.pos_candle_colorpicker, positive_colorpicker).commit();
@@ -38,8 +39,6 @@ public class simulator_settings extends Fragment {
 
         simulator_settings_color_picker shadow_colorpicker = simulator_settings_color_picker.newInstance("Shadow Color");
         getChildFragmentManager().beginTransaction().replace(R.id.shadow_colorpicker, shadow_colorpicker).commit();
-
-
 
         return v;
     }
